38345bb9b8a1e379be191bd670f6a4b3c5f7b551
Addison Emig <addison@kwila.cloud>
2025-12-19 16:30:46 -0500

    chore: add nix flake and update agent guidelines

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Specture - spec-driven software architecture system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pname = "specture";
+        version = "0.1.0";
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          inherit pname version;
+          src = self;
+          # vendorHash locks Go module dependencies
+          vendorHash = "sha256-zrWYKBVCY4HvuxLiPQVJCaNt6P73UOu0jsIoQ2aaLTU=";
+          doCheck = false;
+          meta = with pkgs.lib; {
+            description = "Spec-driven software architecture system";
+            homepage = "https://github.com/specture-system/specture";
+            license = licenses.mit;
+            platforms = platforms.all;
+          };
+        };
+
+        packages.specture = self.packages.${system}.default;
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/specture";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            git
+            golangci-lint
+            just
+            pre-commit
+          ];
+        };
+      }
+    );
+}

5bee5dac3be6bb4a89f7de27d9eea96580ecd0b5
Addison Emig <addison@kwila.cloud>
2025-12-20 05:29:11 -0500

    fix: error handling

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -15,7 +14,7 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		// Cobra already prints the error and usage, so we just exit
 		os.Exit(1)
 	}
 }
